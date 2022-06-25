c5764f6b71e9b14f18d2247ee13205dd6203871d
Konstantin Bakelev <bakons03@mail.ru>
2022-06-25 20:54:05 +0700

    Deleted garbage

--- Lab2/src/Lab2/WorkflowTemplate/CommandWorker/CommandWorker.java
+++
@@ -1,8 +0,0 @@
-package Lab2.WorkflowTemplate.CommandWorker;
-
-import Lab2.WorkflowTemplate.Exception.CommandExecuteException;
-
-public abstract class CommandWorker
-{
-    public abstract void ExecuteCommand(ExecutionContext context) throws CommandExecuteException;
-}
--- Lab2/src/Lab2/WorkflowTemplate/Exception/WorkflowTemplateException.java
+++
@@ -1,9 +0,0 @@
-package Lab2.WorkflowTemplate.Exception;
-
-public class WorkflowTemplateException extends Exception
-{
-    public WorkflowTemplateException()
-    {
-        super();
-    }
-}
